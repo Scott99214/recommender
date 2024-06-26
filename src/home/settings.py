8ad2a286aad282ee4965be0738360f4512d9b71e
Scott99213 <73677242+Scott99213@users.noreply.github.com>
2024-06-26 10:47:23 -0700

    Video 14

--- src/home/settings.py
+++ src/home/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATA_DIR = BASE_DIR / "data"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -39,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'profiles',
+    'movies',
+    'ratings',
 ]
 
 MIDDLEWARE = [
--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandParser
 from django.contrib.auth import get_user_model
 
+from movies.models import Movie
 from home import utils as home_utils
 
 User = get_user_model()
@@ -8,17 +9,29 @@ User = get_user_model()
 class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument("count", nargs='?', default=10, type=int)
+        parser.add_argument("--movies", action="store_true", default=False)
+        parser.add_argument("--users", action="store_true", default=False)
         parser.add_argument("--show-total", action="store_true", default=False)
     
     def handle(self, *args, **options):
         count = options.get('count')
         show_total= options.get('show_total')
-        profiles = home_utils.get_fake_profiles(count=count)
-        new_users = []
-        for profile in profiles:
-            new_users.append(User(**profile))
-        
-        user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
-        print(f"New users: {len(user_bulk)}")
-        if show_total:
-            print(f"Total users: {User.objects.count()}")
+        load_movies= options.get('movies')
+        load_users= options.get('users')
+        if load_movies:
+            movies = home_utils.load_movie_data(limit=count)
+            movies_new = [Movie(**x) for x in movies]
+            movies_bulk = Movie.objects.bulk_create(movies_new, ignore_conflicts=True)
+            print(f"New movies: {len(movies_bulk)}")
+            if show_total:
+                print(f"Total movies: {Movie.objects.count()}")
+        if load_users:
+            profiles = home_utils.get_fake_profiles(count=count)
+            new_users = []
+            for profile in profiles:
+                new_users.append(User(**profile))
+            
+            user_bulk = User.objects.bulk_create(new_users, ignore_conflicts=True)
+            print(f"New users: {len(user_bulk)}")
+            if show_total:
+                print(f"Total users: {User.objects.count()}")
